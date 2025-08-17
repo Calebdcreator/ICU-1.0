039d200ec9757074feb580b06e47cd51057a54d6
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 01:36:00 +0100

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,10 @@
+// 🔧 ICU 1.0 Bot Configuration
+
+module.exports = {
+    prefix: ".", // Command prefix (example: .menu, .help)
+    ownerName: "Caleb Irunna", 
+    ownerNumber: "2347079799769", // Your WhatsApp number without '+'
+    botName: "ICU 1.0 Bot",
+    footer: "Powered by ICU 1.0",
+    version: "1.0.0"
+};
