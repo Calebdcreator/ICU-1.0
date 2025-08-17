b2593de9f809a63d32c19dfc2c8f5f7f4b703de0
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 02:01:32 +0100

    Create Educationmenu.js

---
+++ Commands/Educationmenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*📘 EDUCATION MENU (15 cmds)*
+
+1. .dictionary
+2. .spellcheck
+3. .periodictable
+4. .element
+5. .math
+6. .convert
+7. .sciencefact
+8. .todayinhistory
+9. .synonym
+10. .antonym
+11. .quiz
+12. .riddle
+13. .essay
+14. .explain
+15. .aistudy (🤖 AI Tutor)
+    `);
+};
