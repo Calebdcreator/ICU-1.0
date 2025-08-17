e371be8a12534e2fef76283ed7a4d089c4460965
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 02:00:21 +0100

    Create Animemenu.js

---
+++ Commands/Animemenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*🎌 ANIME MENU (15 cmds)*
+
+1. .anime
+2. .manga
+3. .character
+4. .waifu
+5. .neko
+6. .husbando
+7. .quoteanime
+8. .animepic
+9. .animenews
+10. .animesearch
+11. .mangasearch
+12. .animequote
+13. .topanime
+14. .animestory
+15. .aianime (🤖 AI Anime Image)
+    `);
+};
