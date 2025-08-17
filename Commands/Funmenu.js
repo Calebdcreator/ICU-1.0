7f9193d49972d0495f948a84a19d03fd2e8083ee
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 02:02:13 +0100

    Create Funmenu.js

---
+++ Commands/Funmenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*😂 FUN MENU (15 cmds)*
+
+1. .joke
+2. .meme
+3. .truth
+4. .dare
+5. .roast
+6. .ship
+7. .simprate
+8. .lovecalc
+9. .fact
+10. .8ball
+11. .rps
+12. .guess
+13. .gif
+14. .pickup
+15. .fonts (✒️ Cool Fonts)
+    `);
+};
