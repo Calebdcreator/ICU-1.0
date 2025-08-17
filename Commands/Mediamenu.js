7f215c0434282f5fc4c8c07919efa92d1390bcdf
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 02:04:31 +0100

    Create Mediamenu.js

---
+++ Commands/Mediamenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*🎥 MEDIA MENU (15 cmds)*
+
+1. .ytmp3
+2. .ytmp4
+3. .play
+4. .spotify
+5. .soundcloud
+6. .tiktok
+7. .facebook
+8. .instagram
+9. .twitter
+10. .pinterest
+11. .wallpaper
+12. .news
+13. .movie
+14. .lyrics
+15. .aivideo (🤖 AI Video/Audio Gen)
+    `);
+};
