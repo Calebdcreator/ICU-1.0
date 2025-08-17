2ab186e4e00bd75be8e8addf1f60719a28fcff69
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 01:58:36 +0100

    Create Group menu.js

---
+++ Commands/Group menu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*📂 GROUP MENU (15 cmds)*
+
+1. .add
+2. .kick
+3. .promote
+4. .demote
+5. .tagall
+6. .linkgroup
+7. .setname
+8. .setdesc
+9. .mute
+10. .unmute
+11. .welcome
+12. .goodbye
+13. .antilink
+14. .invite
+15. .viewonce (🔓 Save View Once)
+    `);
+};
