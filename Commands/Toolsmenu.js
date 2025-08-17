b3f94d4157ec40303d2cc836f011eedb51ecf358
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 01:59:26 +0100

    Create Toolsmenu.js

---
+++ Commands/Toolsmenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*🛠️ TOOLS MENU (15 cmds)*
+
+1. .sticker
+2. .toimg
+3. .tomp3
+4. .ssweb
+5. .calc
+6. .qr
+7. .weather
+8. .google
+9. .translate
+10. .shorturl
+11. .removebg
+12. .tinyurl
+13. .define
+14. .lyrics
+15. .stickersteal (🎭 Sticker Stealer)
+    `);
+};
