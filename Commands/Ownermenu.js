b2725b9b740076906880e9efd80c223cb34337f7
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 02:05:13 +0100

    Create Ownermenu.js

---
+++ Commands/Ownermenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*👑 OWNER MENU (15 cmds)*
+
+1. .shutdown
+2. .restart
+3. .bc
+4. .clearchats
+5. .ban
+6. .unban
+7. .block
+8. .unblock
+9. .setpp
+10. .setstatus
+11. .mode
+12. .eval
+13. .listusers
+14. .backup
+15. .aiadmin (🤖 AI Bot Control)
+    `);
+};
