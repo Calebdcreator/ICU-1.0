257696195e20910c225842eb8633661dc0458513
CalebMD <ubachukwuirunna@gmail.com>
2025-08-17 02:03:35 +0100

    Create Adultmenu.js

---
+++ Commands/Adultmenu.js
@@ -0,0 +1,21 @@
+module.exports.run = async (client, msg) => {
+    msg.reply(`
+*🔞 ADULT MENU (15 cmds)*
+
+1. .hentai
+2. .nsfwneko
+3. .ecchi
+4. .adultgif
+5. .boobs
+6. .ass
+7. .anal
+8. .pornhub
+9. .xvideos
+10. .lesbian
+11. .gay
+12. .milf
+13. .rule34
+14. .hentaivid
+15. .aiporn (🤖 AI Adult Gen)
+    `);
+};
